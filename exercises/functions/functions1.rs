a1693ec41fd3b9919382f0c3e3cef41f26a5a9e8
Daan Van der plas <dvan-der@f1r1s11.codam.nl>
2022-06-16 12:16:04 +0200

    create

---
+++ exercises/functions/functions1.rs
@@ -0,0 +1,11 @@
+// functions1.rs
+// Make me compile! Execute `rustlings hint functions1` for hints :)
+
+
+fn main() {
+    call_me("0637195512");
+}
+
+fn call_me(number: &str) {
+    println!("My number is: {}, call me", number);
+}
